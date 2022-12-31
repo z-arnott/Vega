5b80705975bd97bf8d21fe3d0ed08bcaab515303
Randa Hassan <55730204+rhass-uta@users.noreply.github.com>
2022-12-31 10:48:00 -0500

    Update supabase.js

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://katamtzaudnxbudjjbhm.supabase.co'
+const supabaseUrl = 'https://katamtzaudnxbudjjbhm.supabase.co' 
 const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+module.exports =  createClient(supabaseUrl, supabaseKey)
 
-export default
+//ntl env:set SUPABASE_KEY [retrieve key from database API]
