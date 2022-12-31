bd85d1bf68bac6b871fdbbf16d79bbd372fd31d2
Randa Hassan <55730204+rhass-uta@users.noreply.github.com>
2022-12-31 10:55:04 -0500

    Update storageFacade.utils.ts

--- src/utils/storageFacade.utils.ts
+++ src/utils/storageFacade.utils.ts
@@ -1,5 +1,20 @@
+const supabase = require ('./utils/supabase.js');
+console.log(supabase)
+
+
 /****************** STORAGE FACADE API **********************/
 //Receives read requests, returns requested data
+function ReadData{
+  const {public:tablename} = await supabase
+  .from('tablename')
+  .select('columnname1')
+  console.log('tablename', tablename)
+  }
+
+  catch (error){
+    console.log('error', error);
+  }
+}
 //Sends write requests
 //Stores 
 //Transports formatted query requests to VIC
