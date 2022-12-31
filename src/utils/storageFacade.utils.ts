4d51fd638997d0aa7a05e3a1adc4155cea1fa32e
Randa Hassan <55730204+rhass-uta@users.noreply.github.com>
2022-12-31 10:19:23 -0500

    Update storageFacade.utils.ts

--- src/utils/storageFacade.utils.ts
+++ src/utils/storageFacade.utils.ts
@@ -1,1 +1,8 @@
 /****************** STORAGE FACADE API **********************/
+//Receives read requests, returns requested data
+//Sends write requests
+//Stores 
+//Transports formatted query requests to VIC
+//Recieves list of vulnerabilities and sends to database for storage
+
+//fetches data for 
