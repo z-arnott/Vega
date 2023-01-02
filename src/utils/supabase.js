3326b7a54c3c6344c646351aa5da5ebe0bc63e57
Randa Hassan <55730204+rhass-uta@users.noreply.github.com>
2023-01-02 08:23:50 -0500

    Update supabase.js

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -5,3 +5,6 @@ const supabaseKey = process.env.SUPABASE_KEY
 module.exports =  createClient(supabaseUrl, supabaseKey)
 
 //ntl env:set SUPABASE_KEY [retrieve key from database API]
+
+//Function #1: Set sessionID
+const {user, sessionID, error} = await supabase.auth.signUp()
