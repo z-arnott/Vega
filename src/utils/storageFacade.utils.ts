c4708e5e734b7caa647ac0b80ad255804c3d7ff3
Randa Hassan <55730204+rhass-uta@users.noreply.github.com>
2023-01-05 01:25:06 -0500

    adjusted function modifiers

--- src/utils/storageFacade.utils.ts
+++ src/utils/storageFacade.utils.ts
@@ -1,50 +1,50 @@
-// import {sessionID} from '@utils/supabase';
-// import supabase from 'supabase';
-// /****************** SET SESSION ID **********************/
+//import {sessionID} from '@utils/supabase';
+import { supabase } from "./supabase";
+/****************** SET SESSION ID **********************/
 
  
-// /****************** READ PACKAGE **********************/
-// function ReadPackage(sessionID:number, filterparam:any, sortparam:any): any{
-//   let packageresult = await supabase //common syntax on JS: const {data,error} = await...
-//     .from(packages) 
-//     .select('*')
-//     .eq('sessionID',sessionID)
-//   //Step #2: Incorporate filtering
-//     //.eq('packageID','specificpackage); //if removed, displays all packageIDs
-//   //Step #3: Incorporate sorting
-//   //Step #4: Incorporate pagination - see classdiagrams.drawio
-//   return packageresult;
-// }
+/****************** READ PACKAGE **********************/
+export async function ReadPackage(sessionID:number, filterparam:any, sortparam:any){
+  let packageresult = await supabase //common syntax on JS: const {data,error} = await...
+    .from('packages') 
+    .select('*')
+    .eq('sessionID',sessionID)
+  //Step #2: Incorporate filtering
+    //.eq('packageID','specificpackage); //if removed, displays all packageIDs
+  //Step #3: Incorporate sorting
+  //Step #4: Incorporate pagination - see classdiagrams.drawio
+  return packageresult;
+}
 
 // /****************** READ VULNERABILITY **********************/
-// function ReadVulnerability(packageIDrequired:number):any{
-//   let vulnerabilitiesresults = await supabase 
-//     .from('junction')
-//     .select('cveid, packageid, vulnerabilities!inner(description,risk,likelihood,impact)')
-//     .filter('packageid','in',packageIDrequired)
-//   //Step #2: Incorporate filtering
-//     //.eq('packageID','specificpackage); //if removed, displays all packageIDs
-//   //Step #3: Incorporate sorting
-//    // .order('packageID', {ascending:false}) 
-//     //Step #4: Incorporate pagination - see classdiagrams.drawio
-//   return vulnerabilitiesresults;
-//  }
+export async function ReadVulnerability(packageIDrequired:number){
+  let vulnerabilitiesresults = await supabase 
+    .from('junction')
+    .select('cveid, packageid, vulnerabilities!inner(description,risk,likelihood,impact)')
+    .filter('packageid','in',packageIDrequired)
+  //Step #2: Incorporate filtering
+    //.eq('packageID','specificpackage); //if removed, displays all packageIDs
+  //Step #3: Incorporate sorting
+   // .order('packageID', {ascending:false}) 
+    //Step #4: Incorporate pagination - see classdiagrams.drawio
+  return vulnerabilitiesresults;
+ }
 
-// /****************** WRITE PACKAGE**********************/
-// //Function #4: Write Request (Package)
-// function WritePackageRequest(tablename: string, packageIDvalue: number, name: string, sessionIDvalue: string, packageversionvalue: string, consRiskvalue: float, consImpactvalue: float, consLikelihoodvalue:float, highestRiskvalue: float, purlstring: string, cpeNamestring: string): void {
-//   const {error} = await supabase
-//   .from(tablename)
-//   .insert({packageID: packageIDvalue, sessionID: sessionIDvalue, name: namevalue, packageversion : packageversionvalue, consRisk:consRiskvalue, consImpact:consImpactvalue ,consLikelihood: consLikelihoodvale, highestRisk:highestRiskvalue ,purl: purlstring,cpeName:cpeNamestring)
-// }
+/****************** WRITE PACKAGE**********************/
+//Function #4: Write Request (Package)
+export async function WritePackageRequest(tablename: string, packageIDvalue: number, name: string, sessionIDvalue: string, packageversionvalue: string, consRiskvalue: number , consImpactvalue: number, consLikelihoodvalue:number, highestRiskvalue: number, purlstring: string, cpeNamestring: string){
+  const {error} = await supabase
+  .from(tablename)
+  .insert({packageID: packageIDvalue, sessionID: sessionIDvalue, name: name, packageversion : packageversionvalue, consRisk:consRiskvalue, consImpact:consImpactvalue ,consLikelihood: consLikelihoodvalue, highestRisk:highestRiskvalue ,purl: purlstring,cpeName:cpeNamestring});
+}
 
-// /****************** WRITE VULNERABILITY **********************/
-// //Function #5: Write Request (Vulnerability)
-// function WriteVulnRequest(tablename: string, cveidv: number, descriptions:string, riskv: float, likelihoodv: float, impactv: float): void {
-//   const {error} = await supabase
-//   .from(tablename)
-//   .insert({cveid: cveidv, description: descriptions, risk: riskv, likelihood : likelihoodv, impact:impactv)
-// }
+/****************** WRITE VULNERABILITY **********************/
+//Function #5: Write Request (Vulnerability)
+export async function WriteVulnRequest(tablename: string, cveidv: number, descriptions:string, riskv: number, likelihoodv: number, impactv: number){
+  const {error} = await supabase
+  .from(tablename)
+  .insert({cveid: cveidv, description: descriptions, risk: riskv, likelihood : likelihoodv, impact:impactv});
+}
           
 // /****************** WRITE JUNCTION **********************/
 // //consider eliminating junction table
--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -7,14 +7,26 @@ const supabaseKey = process.env.SUPABASE_KEY;
 // Create a single supabase client for interacting with your databasd
 const supabase  =  createClient<Database>(supabaseUrl, supabaseKey!)
 
-export async function getJunction(packageID:number) {
-  return await supabase.from('junction').select('cveid');
-}
-type JunctionResponse = Awaited<ReturnType<typeof getJunction>>
-export type JunctionResponsesuccess = JunctionResponse['data'];
-export type JunctionResponseerror = JunctionResponse['error'];
-
 export {supabase}
+
+
+// export async function getJunction() {
+//   return await supabase.from('junction').select('cveid, packageid');
+// }
+// console.log(getJunction);
+// type JunctionResponse = Awaited<ReturnType<typeof getJunction>>
+// export type JunctionResponsesuccess = JunctionResponse['data'];
+// export type JunctionResponseerror = JunctionResponse['error'];
+// export async function getJunction2(){
+//   const {data:junction, error} = await supabase.from('junction').select('cveid, packageid');
+//   return {
+//     public: {
+//       junction
+//     }
+//   }
+//   }
+
+
 //export {packages};
 //Function #1: Set sessionID
 //const {user, sessionID, error} = await supabase.auth.signUp()
