68cc778eb269b2a0009d61e9cb1abc13ce7e6275
Randa Hassan <55730204+rhass-uta@users.noreply.github.com>
2023-01-10 13:17:40 -0500

    fixed dot.config

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,14 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 import dotenv from 'dotenv';
 
-
-
+dotenv.config();
 const supabaseUrl = 'https://katamtzaudnxbudjjbhm.supabase.co' as string;
 const supabaseKey = process.env.SUPABASE_KEY as string;
 
 
 // Create a single supabase client for interacting with your database
-dotenv.config();
 const supabase  =  createClient(supabaseUrl, supabaseKey)
 
 export {supabase}
