d3f9e982977fb5f93cd42ba9345e9b909a222e62
Randa Hassan <55730204+rhass-uta@users.noreply.github.com>
2023-01-09 23:15:18 -0500

    tests 7-10 about writing and deleting vulnerabilities all looking great!

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,20 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
-import { Database } from './database.types';
 import dotenv from 'dotenv';
+
 
 
 const supabaseUrl = 'https://katamtzaudnxbudjjbhm.supabase.co' as string;
 const supabaseKey = process.env.SUPABASE_KEY as string;
-
 
 // Create a single supabase client for interacting with your database
 dotenv.config();
 const supabase  =  createClient(supabaseUrl, supabaseKey)
 
 export {supabase}
-
-//export {packages};
-//Function #1: Set sessionID
-//const {user, sessionID, error} = await supabase.auth.signUp()
-
-//export {user,sessionID}
