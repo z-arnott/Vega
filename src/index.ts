398835642218c5cbc1c79264372c2c919ebaf256
z-arnott <zoe.arnott@hotmail.com>
2023-01-15 18:20:01 -0500

    add risk analysis endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { Package, SbomFormat } from '../src/utils/types.utils';
-import {parse} from '../src/services/parserContext.services';
+import { parse } from '../src/services/parserContext.services';
+import { AnalysisInfo } from '../src/services/riskAnalysis.services';
 import fileUpload from 'express-fileupload';
 
 const express = require('express');
 const app = express();
 app.use(express.json());
-app.use(fileUpload()); 
+app.use(fileUpload());
 const port = 8088; // default port to listen
 
 //Homepage
@@ -17,18 +18,18 @@ app.get('/', (req: any, res: any) => {
 /*************** Add supported endpoints here ***************/
 
 // define a route handler for the Upload endpoint
-app.post(
-  '/upload',
-  (req: any, res: any, next: any) => {
-    let sbom = req.files.sbom.data.toString('utf8');
-    let sbomType = req.query.format;
-    let packages = parse(sbom, sbomType);
-    console.log(packages);
-    res.send('Upload: parsed ' + packages.length +' packages');
-    //add middleware calls here as needed
-  }
-);
+app.post('/upload', (req: any, res: any, next: any) => {
+  let sbom = req.files.sbom.data.toString('utf8');
+  let sbomType = req.query.format;
+  let packages = parse(sbom, sbomType);
+  console.log(packages);
+  res.send('Upload: parsed ' + packages.length + ' packages');
+  //add middleware calls here as needed
+});
 
+app.get('/riskanalysis', (req: any, res: any, next: any) => {
+  res.send('Risk Analysis reached');
+});
 /*************** Start Server ***************/
 // start the Express server
 app.listen(port, () => {
@@ -40,5 +41,5 @@ process.on('SIGTERM', () => {
   console.log('SIGTERM signal received: closing HTTP server');
   app.close(() => {
     console.log('HTTP server closed');
-  })
-})
+  });
+});
