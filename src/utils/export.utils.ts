73c8de2c6e73d05955bd91936a120e1d44009607
Randa Hassan <55730204+rhass-uta@users.noreply.github.com>
2023-01-30 23:33:06 -0500

    extended elements based on package/vulnerability count + added a test

--- src/utils/export.utils.ts
+++ src/utils/export.utils.ts
@@ -1,7 +1,22 @@
 import {PackageViewParam, severityRating, DisplayPackage, Vulnerability}  from './types.utils';
-import { readPackagesDashboard } from "./storageFacade.utils";
+import { readPackagesDashboard,countVulnerabilities, countPackages } from "./storageFacade.utils";
+import { numericLiteral } from '@babel/types';
 /****************** EXPORT API **********************/
- export async function exportResults(sessionId:number){
-    let file = JSON.stringify(await readPackagesDashboard(sessionId, PackageViewParam.NUMBER_OF_VULNERABILITIES,['LOW','MEDIUM','HIGH','CRITICAL'],1, 5000000000));
+/**
+ * Read all packages in a session
+ * @param sessionid associated with one user
+ * @returns json of packages and nested vulnerabilitiies assoicated with id
+ */
+export async function exportResults(sessionId:number){
+   //calculate number of vulnerabilities
+    let number = await countVulnerabilities(sessionId)* await countPackages(sessionId ); //to give extra padding whether page size denotes packages or vulnerabilities
+    if (number <1){
+        let number =1;
+    }
+    //number is passed as an optional parameter of PAGE_SIZE to stringify all packages and vulnerabilities at once
+    let file = JSON.stringify(await readPackagesDashboard(sessionId, PackageViewParam.NUMBER_OF_VULNERABILITIES,['LOW','MEDIUM','HIGH','CRITICAL'],1, number));
     return file;
+
+    //current version outputs does not output any packages that do not have associated vulnerabilities
+    //discuss whether that's okay or if packages with no vulnerabilities should still be identified
 }
